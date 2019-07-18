9d7177e691ce7b7863dedc9ae466270a7ad8e77a
hendisantika <hendisantika@yahoo.co.id>
2019-07-18 07:49:15 +0700

    #6 - Add PersonController

--- src/main/java/com/hendisantika/springbootcucumber/controller/PersonController.java
+++ src/main/java/com/hendisantika/springbootcucumber/controller/PersonController.java
@@ -1,6 +1,9 @@
 package com.hendisantika.springbootcucumber.controller;
 
 import com.hendisantika.springbootcucumber.model.Person;
+import com.hendisantika.springbootcucumber.service.PersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
